e889756068edd7146fdf50857770417e44593999
ryanmbigelow <ryanmbigelow@gmail.com>
2022-12-04 13:32:30 -0600

    fixed functionality of the update button

--- events/formEvents.js
+++ events/formEvents.js
@@ -23,21 +23,21 @@ const formEvents = () => {
     }
 
     // TODO: CLICK EVENT FOR EDITING A BOOK
-    // if (e.target.id.includes('update-book')) {
-    //   const [, firebaseKey] = e.target.id.split('--');
-    //   const payload = {
-    //     title: document.querySelector('#title').value,
-    //     description: document.querySelector('#description').value,
-    //     image: document.querySelector('#image').value,
-    //     price: document.querySelector('#price').value,
-    //     author_id: document.querySelector('#author_id').value,
-    //     sale: document.querySelector('#sale').checked,
-    //     firebaseKey,
-    //   };
-    //   updateBook(payload).then(() => {
-    //     getBooks(user.uid).then(showBooks);
-    //   });
-    // }
+    if (e.target.id.includes('update-word-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      const payload = {
+        title: document.querySelector('#title').value,
+        definition: document.querySelector('#definition').value,
+        language_id: document.querySelector('#language_id').value,
+        language: document.querySelector('#language_id').value,
+        userId: 'userId',
+        dateSubmitted: 'dateSubmitted',
+        firebaseKey,
+      };
+      updateWord(payload).then(() => {
+        getWords().then(showVocabWords);
+      });
+    }
   });
 };
 
